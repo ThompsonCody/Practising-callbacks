2df2c92423d319e19e0632daf1dc437e2beef767
ThompsonCody <c.williamthompson@gmail.com>
2017-12-01 00:17:30 +0000

    ...

--- adv-sorting.js
+++ adv-sorting.js
@@ -5,26 +5,35 @@ numbers.sort(function(a,b){
 });
 //console.log(numbers);*/
 
-var students = [
+const students = [
   { id: 1, name: "bruce",    age: 40 },
   { id: 2, name: "zoidberg", age: 22 },
   { id: 3, name: "alex",     age: 22 },
   { id: 4, name: "alex",     age: 30 }
-];
-console.log(students[0].age);
+  ];
 
-students.sort(function(a,b){
-  if (a.name< b.name) {
+students.sort((a,b) => {
+  if (a.name < b.name) {
     return -1;
   }
-  if(a.name> b.name)
+  if(a.name > b.name)
     return 1;
 
   if (a.name === b.name) {
       return b.age - a.age;
   }
-
- return 0;
+  /*
+    switch(a.name < b.name){
+      case a.name > b.name:
+        return 1;
+        break;
+      case a.name === b.name:
+        return b.age - a.age;
+        break;
+      default:
+        return -1;
+    }
+  */
+    return 0;
 });
-
-console.log(students);
+  console.log(students);
